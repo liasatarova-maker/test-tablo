99d6dbeef7869d23cc919c44379fda61a1501fd4
liasatarova-maker <liasatarova@gmail.com>
2026-09-23 13:15:31 +0400

    Add Firebase messaging service worker

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB_QxansNQmXsi3_S-1vhzh7Q_EH9_1fQE",
+  authDomain: "tablo-1ad7f.firebaseapp.com",
+  projectId: "tablo-1ad7f",
+  storageBucket: "tablo-1ad7f.firebasestorage.app",
+  messagingSenderId: "1063984512841",
+  appId: "1:1063984512841:web:d629d87538560cbb77a498"
+});
+
+firebase.messaging();
