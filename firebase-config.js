8f7d662d32cdd78cdf84dbd9919e05631fae6326
liasatarova-maker <liasatarova@gmail.com>
2026-09-23 13:15:28 +0400

    Add Firebase push config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const LUXPRINT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB_QxansNQmXsi3_S-1vhzh7Q_EH9_1fQE",
+  authDomain: "tablo-1ad7f.firebaseapp.com",
+  projectId: "tablo-1ad7f",
+  storageBucket: "tablo-1ad7f.firebasestorage.app",
+  messagingSenderId: "1063984512841",
+  appId: "1:1063984512841:web:d629d87538560cbb77a498"
+};
+const LUXPRINT_VAPID_KEY = "BF0Aj6fqmMqjc_iTFSuQSdRpIdkjOUh3U9dJcLmDJpv8AdE2csg-8L5BPVN-KPRj1wgX_GE_hXGQIrZsU4oWUBs";
